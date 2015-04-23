63c3977c3c90c3e0146d1749ebffdb44cc6abdc1
phuocnh88 <phuocnh88@gmail.com>
2015-04-23 17:06:57 +0700

    + Add profile page: currency, upgrade, customize

---
+++ Sources/Assets/Scripts/StoreCustomizePage.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreCustomizePage : MonoBehaviour {
+
+    public int m_NumberOfOption;
+    public GameObject m_ItemSelectPagePrefab;
+    public GameObject m_ItemPrefab;
+
+	// Use this for initialization
+	void Start () { 
+        InitPage();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void InitPage()
+    {
+        GameObject panel = transform.FindChild("Panel").gameObject;
+        for (int i = 0; i < m_NumberOfOption; i++)
+        {
+            GameObject go = (GameObject)GameObject.Instantiate(m_ItemSelectPagePrefab);
+            go.transform.parent = panel.transform;
+            go.transform.localScale = new Vector3(1, 1, 1);
+            go.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -150 - i * 200, 0);
+
+            for (int j = 0; j < 10; j++)
+            {
+                GameObject panel1 = go.transform.FindChild("Panel").gameObject;
+                GameObject go1 = (GameObject)GameObject.Instantiate(m_ItemPrefab);
+                go1.transform.parent = panel1.transform;
+                go1.transform.localScale = new Vector3(1, 1, 1);
+                go1.GetComponent<RectTransform>().anchoredPosition = new Vector3(200 + 0 + j * 200, 0, 0);
+            }
+        }
+        panel.GetComponent<RectTransform>().sizeDelta = new Vector2(1440, m_NumberOfOption * 200);
+    }
+}
---
+++ Sources/Assets/Scripts/StoreTab.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Image = UnityEngine.UI.Image;
+
+public class StoreTab : MonoBehaviour {
+
+    public Sprite[] m_SpriteList;
+    public GameObject m_AchievementTab;
+    public GameObject m_HistoryTab;
+    public GameObject m_CustomizeTab;
+    
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnCurrency()
+    {
+        Image img = gameObject.GetComponent<Image>();
+        img.sprite = m_SpriteList[0];
+
+        
+    }
+
+    public void OnUpgrade()
+    {
+        Image img = gameObject.GetComponent<Image>();
+        img.sprite = m_SpriteList[1];
+
+        
+    }
+
+    public void OnCustomize()
+    {
+        Image img = gameObject.GetComponent<Image>();
+        img.sprite = m_SpriteList[2];
+
+        
+    }
+}
